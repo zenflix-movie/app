3c80db02df79ef278f1ebed46d86278c32611dd8
joseguti <jlgutman@gmail.com>
2026-06-15 16:59:09 -0500

    Adding data for the video streaming platform

---
+++ drizzle/data_scripts/zenflix_category_202606151542.sql
@@ -0,0 +1,21 @@
+INSERT INTO public.zenflix_category (name,description,"createdAt","updatedAt") VALUES
+	 ('Family',NULL,'2026-06-13 04:04:36.569263-05',NULL),
+	 ('Comedy',NULL,'2026-06-13 04:04:36.569263-05',NULL),
+	 ('Animation',NULL,'2026-06-13 04:04:36.569263-05',NULL),
+	 ('Adventure',NULL,'2026-06-13 04:04:36.569263-05',NULL),
+	 ('Fantasy',NULL,'2026-06-13 04:04:36.635048-05',NULL),
+	 ('Romance',NULL,'2026-06-13 04:04:37.135919-05',NULL),
+	 ('Drama',NULL,'2026-06-13 04:04:37.199278-05',NULL),
+	 ('Crime',NULL,'2026-06-13 04:21:14.329867-05',NULL),
+	 ('Action',NULL,'2026-06-13 04:21:14.329867-05',NULL),
+	 ('Horror',NULL,'2026-06-13 04:21:15.618648-05',NULL);
+INSERT INTO public.zenflix_category (name,description,"createdAt","updatedAt") VALUES
+	 ('Thriller',NULL,'2026-06-13 04:21:15.729704-05',NULL),
+	 ('Mystery',NULL,'2026-06-13 04:21:15.743281-05',NULL),
+	 ('War',NULL,'2026-06-13 04:21:17.21476-05',NULL),
+	 ('Science Fiction',NULL,'2026-06-13 04:21:18.103473-05',NULL),
+	 ('History',NULL,'2026-06-13 04:21:25.645619-05',NULL),
+	 ('Documentary',NULL,'2026-06-13 04:21:30.495639-05',NULL),
+	 ('Music',NULL,'2026-06-13 04:21:38.519762-05',NULL),
+	 ('Western',NULL,'2026-06-13 04:21:52.256833-05',NULL),
+	 ('TV Movie',NULL,'2026-06-13 04:26:17.239487-05',NULL);
